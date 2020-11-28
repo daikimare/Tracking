f8ed5e4c2c29a9c1cf2e112c8093e28826d7ab0a
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-11-29 02:23:41 +0900

    first commit

---
+++ JS/main.js
@@ -0,0 +1,56 @@
+// 2で配置したモデルを読み込む。
+Promise.all([
+  faceapi.nets.ssdMobilenetv1.load('./models'), // 精度の高い顔検出モデル
+  faceapi.nets.faceLandmark68Net.load('./models'), // 顔の68個のランドマークの検出モデル
+]).catch((e) => {
+  console.log(`face-apiを読み込むことができませんでした。${e}`);
+});
+
+// Webカメラの起動
+const video = document.getElementById('video');
+const media = navigator.mediaDevices.getUserMedia({
+  audio: false,
+  video: {
+      width: 640,
+      height: 360,
+      aspectRatio: 1.77,
+      facingMode: "user",
+  }
+}).then((stream) => {
+  video.srcObject = stream;
+  video.onloadeddata = () => {
+      video.play();
+  }
+}).catch((e) => {
+  console.log(e);
+});
+
+// 描画用canvasの設定
+const cvs = document.getElementById('canvas');
+const ctx = cvs.getContext('2d');
+cvs.width = 640; cvs.height = 360;
+
+// face-apiで顔のランドマークを取得します。
+let faceData;
+async function getLandMarks(){
+  faceData = await faceapi.detectSingleFace(video).withFaceLandmarks();
+
+  if(faceData == null) return;
+  drawLandMarks(faceData.landmarks.positions);
+}
+
+// 取得したランドマークをcanvasに描画します。
+function drawLandMarks(positions) {
+  ctx.clearRect(0, 0, cvs.width, cvs.height);
+  ctx.fillStyle = '#ffffff';
+  for(let i = 0; i < positions.length; i++){
+      ctx.fillRect(positions[i].x, positions[i].y, 3, 3)
+  }
+}
+
+function render(){
+  requestAnimationFrame(render);
+  getLandMarks();
+}
+
+render();
